a3f9da6a76a9843167afd34c5c285e19b715a4aa
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2023-09-26 16:41:15 -0700

    carrying things kinda works

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,10 @@ extends CharacterBody3D
 @export var SPEED = 5.0
 #const JUMP_VELOCITY = 4.5
 
+@onready var object_hold_position = $ObjectHoldPosition
+var objects_in_range = []
+var held_object
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -23,6 +27,15 @@ func _physics_process(delta):
 	# Add the gravity.
 #	if not is_on_floor():
 #		velocity.y -= gravity * delta
+
+	# Handle held objects
+	if Input.is_action_just_pressed("interact"):
+		if objects_in_range.size() > 0 and not held_object: # only hold one thing at a time
+			held_object = objects_in_range[0] # NOTE: change later?
+			held_object.freeze_mode = RigidBody3D.FREEZE_MODE_KINEMATIC
+
+	if held_object:
+		held_object.global_transform.origin = object_hold_position.global_transform.origin
 
 	# Handle Jump.
 #	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -44,3 +57,11 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _on_item_detection_body_entered(body):
+	if body != self:
+		objects_in_range.append(body)
+	# NOTE: give these objects an outline later?
+
+func _on_item_detection_body_exited(body):
+	objects_in_range.erase(body)
